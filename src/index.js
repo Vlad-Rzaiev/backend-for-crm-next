a15aafee2f8fcc477d29c1f5a810040130d4779a
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2026-07-08 14:19:45 +0200

    create dev branch

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-import express from 'express';
+import { startServer } from './server';
 
-const app = express();
-
-const PORT = 7777;
-
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World!',
-  });
-});
-
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Not Found!',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong!',
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
---
+++ src/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import pino from 'pino';
+import cors from 'cors';
+
+const PORT = process.env.PORT;
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not Found!',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong!',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};

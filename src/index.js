4f0b53f203914d7b4e53f1be0245e19abca05fbf
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2026-07-08 13:43:17 +0200

    midlewares

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-const message = "Hello NODE";
+import express from 'express';
 
-console.log(message);
+const app = express();
+
+const PORT = 7777;
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Not Found!',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
